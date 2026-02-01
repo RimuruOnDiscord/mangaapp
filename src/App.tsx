b3d5cd0db44486362807b90fce58ff1bde65a836
Rimuru <velocityontopfrfr@gmail.com>
2026-02-01 13:23:20 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
+
 import Home from './pages/Home';
-import Browse from './pages/Browse'; // 1. Import Browse
-import Page from './pages/Page';   // 2. Import Read
-import Manga from './pages/Manga';   // 2. Import Read
+import Browse from './pages/Browse'; 
+import Page from './pages/Page';   
+import Manga from './pages/Manga';   
+import Random from './pages/Random';
+
+const PageWrapper = ({ children }: { children: React.ReactNode }) => (
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.3, ease: "easeInOut" }}
+    className="min-h-screen bg-[#050505]"
+  >
+    {children}
+  </motion.div>
+);
 
 function AppContent() {
   const location = useLocation();
@@ -12,69 +26,27 @@ function AppContent() {
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        {/* HOME ROUTE */}
-        <Route
-          path="/"
-          element={
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="min-h-screen bg-[#050505]"
-            >
-              <Home />
-            </motion.div>
-          }
-        />
+        {/* HOME */}
+        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+        
+        {/* BROWSE */}
+        <Route path="/browse" element={<PageWrapper><Browse /></PageWrapper>} />
+        
+        {/* NEWEST - Sorted by Release Date */}
+        <Route path="/newest" element={<PageWrapper><Browse initialSort="start_date" title="Newest Releases" /></PageWrapper>} />
+        
+        {/* UPDATED - Sorted by Chapters/Status */}
+        <Route path="/updated" element={<PageWrapper><Browse initialSort="chapters" title="Recently Updated" /></PageWrapper>} />
+        
+        {/* ADDED - Sorted by MAL_ID (Database entry order) */}
+        <Route path="/added" element={<PageWrapper><Browse initialSort="mal_id" title="Recently Added" /></PageWrapper>} />
+        
+        {/* RANDOM */}
+        <Route path="/random" element={<PageWrapper><Random /></PageWrapper>} />
 
-        {/* BROWSE ROUTE - This was the missing piece! */}
-        <Route
-          path="/browse"
-          element={
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="min-h-screen bg-[#050505]"
-            >
-              <Browse />
-            </motion.div>
-          }
-        />
-
-
-        <Route
-          path="/read/:mangaId/chapter/:chapterId"
-          element={
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="min-h-screen bg-[#050505]"
-            >
-              <Page />
-            </motion.div>
-          }
-        />
-
-        {/* READ ROUTE - This was the missing piece! */}
-        <Route
-          path="/read/:mangaId"
-          element={
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="min-h-screen bg-[#050505]"
-            >
-              <Manga />
-            </motion.div>
-          }
-        />
+        {/* INFO & READER */}
+        <Route path="/read/:mangaId" element={<PageWrapper><Manga /></PageWrapper>} />
+        <Route path="/read/:mangaId/chapter/:chapterId" element={<PageWrapper><Page /></PageWrapper>} />
       </Routes>
     </AnimatePresence>
   );
